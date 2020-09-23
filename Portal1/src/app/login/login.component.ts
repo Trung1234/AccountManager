a2a907de60a1c420730e61e460cbd48deec9701a
Trung <trungsieunhan24@gmail.com>
2020-09-23 14:40:03 +0700

    add toast for login

--- Portal1/src/app/login/login.component.ts
+++ Portal1/src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../shared/user.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 
 @Component({
@@ -15,7 +17,7 @@ export class LoginComponent implements OnInit {
     UserName: '',
     Password: ''
   }
-  constructor(private service: UserService, private router: Router) { }
+  constructor(private service: UserService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit() {
     if (localStorage.getItem('token') != null)
@@ -25,11 +27,15 @@ export class LoginComponent implements OnInit {
   onSubmit(form: NgForm) {
     this.service.login(form.value).subscribe(
       (res: any) => {
+        this.toastr.success('Login successully');
         localStorage.setItem('token', res.token);
         this.router.navigateByUrl('/home');
       },
       err => {
-        console.log(err);
+        if (err.status == 400)
+          this.toastr.error('Incorrect username or password.', 'Authentication failed.');
+        else
+          console.log(err);
 
       }
     );
